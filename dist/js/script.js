c7494ce6c01b6d7c569ef09d19de2e998748fdb9
Gvozdoedko <uraradhenko007@gmail.com>
2022-05-03 19:30:07 +0300

    calculator V2.1

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,46 +1,47 @@
 const question = prompt(`Выберите операцию:
 +, -, *, /, %, cos, sin, pow`);
+let firstNumber;
+let secondNumber;
+let firstArr = ["+", "-", "*", "/", "%"];
+let secondArr = ["cos", "sin"];
+let thirdArr = ["pow"];
 
-if (question == '+') {
-    const firstNumber = prompt('Введите первое число');
-    const secondNumber = prompt('Введите первое число');
+{
+    if (question == firstArr[0] || question == firstArr[1] || question == firstArr[2] || question == firstArr[3] || question == firstArr[4]) {
+        firstNumber = prompt('Введите первое число');
+        secondNumber = prompt('Введите второе число');
+    } else if (question == secondArr[0] || question == secondArr[1]) {
+        firstNumber = prompt('Введите число');
+    } else if (question == thirdArr[0]) {
+        firstNumber = prompt('Введите число');
+        secondNumber = prompt('Введите степень');
+    }
+}
+
+if (question == firstArr[0]) {
     const sumOfNum = +firstNumber + +secondNumber;
     alert(`Sum:  ${firstNumber} + ${secondNumber}  = ${sumOfNum}`);
-} else if (question == '-') {
-    const firstNumber = prompt('Введите первое число');
-    const secondNumber = prompt('Введите первое число');
+} else if (question == firstArr[1]) {
     const diffOfNum = firstNumber - secondNumber;
     alert(`Diff: ${firstNumber} - ${secondNumber}  = ${diffOfNum}`);
-} else if (question == '*') {
-    const firstNumber = prompt('Введите первое число');
-    const secondNumber = prompt('Введите первое число');
+} else if (question == firstArr[2]) {
     const multOfNum = firstNumber * secondNumber;
     alert(`Multi: ${firstNumber} * ${secondNumber}  = ${multOfNum}`);
-} else if (question == '/') {
-    const firstNumber = prompt('Введите первое число');
-    const secondNumber = prompt('Введите первое число');
+} else if (question == firstArr[3]) {
     const diffOfNum = firstNumber / secondNumber;
     alert(`Div: ${firstNumber} / ${secondNumber}  = ${diffOfNum}`);
-} else if (question == '%') {
-    const firstNumber = prompt('Введите первое число');
-    const secondNumber = prompt('Введите первое число');
+} else if (question == firstArr[4]) {
     const remNum = firstNumber % secondNumber;
     alert(`Rem: ${firstNumber} % ${secondNumber}  = ${remNum}`);
-} else if (question == 'cos') {
-    const firstNumber = prompt('Введите число');
+} else if (question == secondArr[0]) {
     const cosNum = Math.cos(firstNumber);
     alert(`Cos: = ${cosNum}`);
-} else if (question == 'sin') {
-    const firstNumber = prompt('Введите число');
-    const sinNum = Math.cos(firstNumber);
+} else if (question == secondArr[1]) {
+    const sinNum = Math.sin(firstNumber);
     alert(`Sin: = ${sinNum}`);
-} else if (question == 'pow') {
-    const firstNumber = prompt('Введите число');
-    const secondNumber = prompt('Введите степень');
+} else if (question == thirdArr[0]) {
     const powNum = Math.pow(firstNumber, secondNumber);
     alert(`Pow: = ${powNum}`);
 }
-  
 
-
-
+console.log(secondArr[0], secondArr[1], thirdArr[0]);
